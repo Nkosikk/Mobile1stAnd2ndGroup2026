b3e81d67ba15da32c97865e611618bafab4541c5
Nkosi <njceles@gmail.com>
2026-07-06 19:48:38 +0200

    created first android test

--- src/test/java/StartNdosiMobileApp.java
+++ src/test/java/StartNdosiMobileApp.java
@@ -1,4 +1,41 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class StartNdosiMobileApp {
+
+    public static AndroidDriver driver;
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void lauchNdosiQAApp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", System.getProperty("user.dir") + "/src/main/Apps/app-qa-release.apk");
+
+        driver= new AndroidDriver(new URL("http://172.20.10.7:4723/"), capabilities);
+    }
+
+    @After
+    public void quitApp(){
+        System.out.println("App successfully started");
+        driver.findElement(By.id("com.ndosi.mobile:id/btnClose")).click();
+    }
+
+
+
 
 
 }
