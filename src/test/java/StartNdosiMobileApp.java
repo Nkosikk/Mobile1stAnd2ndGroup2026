17f12dbe9d01a15e00cd02ac9cc73ee5c587679d
Nkosi <njceles@gmail.com>
2026-07-07 15:10:19 +0200

    added capabilities

--- src/test/java/StartNdosiMobileApp.java
+++ src/test/java/StartNdosiMobileApp.java
@@ -1,13 +1,12 @@
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
 
 public class StartNdosiMobileApp {
 
@@ -15,23 +14,26 @@ public class StartNdosiMobileApp {
 
     @Before
     public void setup() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("automationName", "UiAutomator2");
-        capabilities.setCapability("app", System.getProperty("user.dir") + "/src/main/Apps/app-qa-release.apk");
 
-        driver= new AndroidDriver(new URL("http://172.20.10.7:4723/"), capabilities);
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setPlatformName("Android")
+                .setAutomationName("UiAutomator2")
+                .setApp(System.getProperty("user.dir") + "/src/main/Apps/app-qa-release.apk");
+
+        driver = new AndroidDriver(URI.create("http://10.0.0.168:4723/").toURL(), options);
     }
 
     @Test
-    public void lauchNdosiQAApp() throws MalformedURLException {
-        driver.findElement(By.id("com.ndosi.mobile:id/btnClose")).click();
+    public void lauchNdosiQAApp() throws InterruptedException {
+        Thread.sleep(5000); // Wait for the app to load
+        driver.findElement(By.xpath("//android.widget.Button")).click();
     }
 
     @After
     public void quitApp(){
-        System.out.println("App successfully started");
-
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 
