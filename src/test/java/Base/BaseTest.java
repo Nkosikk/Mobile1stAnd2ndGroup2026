e8a5d7716f8ba5d2514a304a5e9a804ce077aee2
AvelaNoganta <avisnamsisi@gmail.com>
2026-07-13 19:17:57 +0200

    Added the Base Class

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -1,4 +1,29 @@
 package Base;
 
+import Utilities.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class BaseTest {
+
+    protected AppiumDriver driver;
+    protected Properties config;
+
+    public void setUpAndLogin() throws IOException {
+        config = new Properties();
+        FileInputStream fis = new FileInputStream(
+                System.getProperty("user.dir") + "src/test/resources/configs/config.properties");
+        config.load(fis);
+
+        DriverFactory.initDriver(config);
+        driver = DriverFactory.getDriver();
+
+
+    }
+
+
 }
