e48a308970edecadbc3cd16302d91cdce0034cf5
AvelaNoganta <avisnamsisi@gmail.com>
2026-07-13 20:21:23 +0200

    Added the Login Page

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -4,7 +4,6 @@ import Utilities.DriverFactory;
 import io.appium.java_client.AppiumDriver;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
