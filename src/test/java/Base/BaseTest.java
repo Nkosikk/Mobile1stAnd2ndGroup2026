04f910fbe92e5e92b77d06c24dc24ad3f22d4ac0
AvelaNoganta <avisnamsisi@gmail.com>
2026-07-13 21:09:23 +0200

    Added comments to pages

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -1,3 +1,4 @@
+// Declares the package for the base test class
 package Base;
 
 import Utilities.DriverFactory;
@@ -7,22 +8,28 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+// Defines the BaseTest class which serves as a base class for all test classes
 public class BaseTest {
 
+    // Declares a protected AppiumDriver variable that can be accessed by subclasses
     protected AppiumDriver driver;
+    // Declares a protected Properties variable to store configuration settings accessible by subclasses
     protected Properties config;
 
+    // Method that sets up the driver and loads configuration before running tests
     public void setUpAndLogin() throws IOException {
+        // Initializes a new Properties object to store configuration data
         config = new Properties();
+        // Creates a FileInputStream to read the configuration properties file from the resources directory
         FileInputStream fis = new FileInputStream(
                 System.getProperty("user.dir") + "src/test/resources/configs/config.properties");
+        // Loads the properties from the configuration file into the Properties object
         config.load(fis);
 
+        // Initializes the AppiumDriver using the DriverFactory with the loaded configuration
         DriverFactory.initDriver(config);
+        // Retrieves the initialized driver from the DriverFactory and assigns it to the driver variable
         driver = DriverFactory.getDriver();
-
-
     }
-
 
 }
--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,3 +1,4 @@
+// Declares the package for this Page Object Model class
 package Pages;
 
 import io.appium.java_client.AppiumDriver;
@@ -5,26 +6,35 @@ import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 import java.util.Properties;
 
 public class LoginPage {
 
+    // Declares an AppiumDriver variable to control the mobile application
     AppiumDriver driver;
+    // Declares a Properties variable to store configuration settings
     Properties config;
+    // Declares a WebDriverWait variable for explicit waits on elements
     WebDriverWait wait;
 
+    // Constructor that initializes the LoginPage with an AppiumDriver and configuration Properties
     public LoginPage(AppiumDriver driver, Properties config)
     {
+        // Assigns the passed AppiumDriver to the instance variable
         this.driver = driver;
+        // Assigns the passed Properties to the instance variable
         this.config = config;
-        //implicit wait
+        // Creates a WebDriverWait instance with a timeout of 20 seconds for explicit waits
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        // Initializes page elements using PageFactory and AppiumFieldDecorator for Appium support
         PageFactory.initElements(new AppiumFieldDecorator(driver),this);
     }
 
+    // Defines a By locator for the burger menu button in native applications
     private By burgerMenuButtonNative = By.xpath("//android.widget.Button");
+    // Defines a By locator for the burger menu button in web applications
     private By burgerMenuButtonWeb = By.xpath("//button[@class='nav-burger']");
 
+    // End of LoginPage class
 }
